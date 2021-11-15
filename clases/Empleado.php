19ad397e02ba332cc9c96f908525b604ef709fb8
JhoselinTumiri <74874391+JhoselinTumiri@users.noreply.github.com>
2021-11-15 19:53:07 +0100

    "Administrador:Jhoselin:claseAbstractEmpleado"

---
+++ clases/Empleado.php
@@ -0,0 +1,47 @@
+<?php 
+//abstract class Empleado (comentado para probar)
+class Empleado{
+    private $nombre;
+    private $apellido;
+    private $numeroSeguridadSocial;
+
+    function __construct($nombre, $apellido, $numeroSeguridadSocial )
+    {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->numeroSeguridadSocial = $numeroSeguridadSocial;
+    }
+
+    function getNombre(){
+        return $this->nombre;
+    }
+    function getApellido(){
+        return $this->apellido;
+    }
+    function getNumeroSeguridadS(){
+        return $this->numeroSeguridadSocial;
+    }
+    function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+    function setApellido($apellido){
+        $this->apellido = $apellido;
+    }
+    function setNumeroSeguridadS($numeroSeguridadSocial){
+        $this->numeroSeguridadSocial = $numeroSeguridadSocial;
+    }
+
+    function mostrar(){
+        return "Este empleado  $this->nombre $this->apellido con el NSS: $this->numeroSeguridadSocial";
+    }
+
+   /* abstract function ingresos(){
+
+     }*/
+
+}
+
+
+
+
+?>
