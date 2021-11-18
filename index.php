a05157870be3947d19c89be6b11eff2afc56b190
JhoselinTumiri <74874391+JhoselinTumiri@users.noreply.github.com>
2021-11-18 01:10:38 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,18 +7,18 @@ include "clases/PruebaPolimorf.php";
 
 //Prueba de datos
 $empleado1= new EmpleadoPlantilla("Lola", "Etxegaray", "14-78451589-75",40,10);
-$empleadoPC = new EmpleadoPorComision("Maria", "Behobide", "18-56895478-56",50,300,10);
+$empleadoPC = new EmpleadoPorComision("Maria", "Behobide", "18-56895478-56",5,30,10);
 
 //llamada al método mostrar
 $resultado = "<b>Empleado:</b></br></br> ";
-$resultado .= "<p><b>Empleado Asalariado:</b></br> ".$empleado1->mostrar().$empleado1->ingresos()."</p></br>";
+$resultado .= "<p><b>Empleado Asalariado:</b></br> ".$empleado1->mostrar()."</br>Los ingresos son: ".$empleado1->ingresos()."</p></br>";
 
-$resultado .= "<p><b>Empleado Por Comisión:</b></br> ".$empleadoPC->mostrar().$empleadoPC->ingresos()."</br>";
+$resultado .= "<p><b>Empleado Por Comisión:</b></br> ".$empleadoPC->mostrar()."</br>Los ingresos son: ".$empleadoPC->ingresos()."</br>";
 
 $resultado .= "<p><b>Prueba Polimorfismo:</b></br>Los ingresos de ". $empleadoPC->getApellido() ." son ". PruebaPolimorf::calcular($empleadoPC)."</br>";
 $resultado .= "Los ingresos de ". $empleado1->getApellido() ." son ". PruebaPolimorf::calcular($empleado1)."</br></p>";
 
-/*
+
 $empleado3 = new EmpleadoPlantilla("Karmele", "Arocena", "22-45561256-20",10,5);
 $empresa =new Empresa();
 //AÑADIR EMPLEADOS
@@ -26,6 +26,8 @@ $empresa->addEmpleado($empleado1);
 $empresa->addEmpleado($empleadoPC);
 $empresa->addEmpleado($empleado3);
 $resultado .= "<p><b>Listado Empresa:</b></br> ". $empresa->listarEmpleados() ."</br>";
-*/
+
+$resultado .= "<p><b>La suma total de ingresos es: </b></br> ". $empresa->sumaIngresos() ."</br>";
+
 include "includes/vista_resultado.php";
 ?>
